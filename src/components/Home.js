456113f2c8f8ea3db30b2ea684c9698de990219f
seyelence <infinityx14@gmail.com>
2021-11-11 17:52:07 -0500

    fixed scrolling, need to fix url situation, finish styling, button for resume most likely soon.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,13 @@ import React from "react";
 import './Home.css'
 
 function Home() {
-        return (
-          <section id = "index">
-          <div>
-          <h1 className = "title">    Hello World! </h1>
-      <section className = "desc"> My name is Thamany Valbrune, and I am a junior at University of Massachusetts- Boston. 
-      My interests are within web development and cybersecurity. Check out my links and stuff! </section>
+
+  return (
+    <section id = "index">
+    <div>
+        <h1 className = "title">    Hello World! </h1>
+          <section className = "desc"> My name is Thamany Valbrune, and I am a junior at University of Massachusetts- Boston. 
+        My interests are within web development and cybersecurity. Check out my links and stuff! </section>
       </div>
       </section>
         );   
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,67 +1,31 @@
+import { Card } from "@material-ui/core";
 import React from "react";
-import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
 import { Link } from "react-router-dom";
+import './Projects.css';
 
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: [
-      'Roboto'
-    ],
-    h4: {
-      fontWeight: 600,
-      fontSize: 28,
-      lineHeight: '2rem',
-      },
-    h5: {
-      fontWeight: 100,
-      lineHeight: '2rem',
-    },
-  },
-});
-
-const styles = makeStyles({
-  wrapper: {
-    width: "65%",
-    margin: "auto",
-    textAlign: "center"
-  },
-  bigSpace: {
-    marginTop: "10rem",
-    
-  },
-  littleSpace:{
-    marginTop: "2.5rem",
-    margin: "15rem",
-  },
-  grid:{
-    display: "flex", 
-    justifyContent: "center",
-    alignItems: "center",
-    flexWrap: "wrap", 
-  },
-})
 function Projects() {
-  const classes = styles(); 
   return (
     <section id="projects">
-    <div className="home">
-      <div class="container">
-
-          <div class="classes.wrapper">
-            <h1 className={classes.bigSpace}> Projects </h1>
-            <p>
-       Checkout some of my projects below! Will update as I gain more experience and create new things.
-            </p>
-            <h3> Smart Game AI </h3>
-            <a href="https://github.com/seyelence/cs310/tree/main/pa3" rel="noreferrer">
-            Check it out on GitHub </a>
-
-          </div>
-          <br/>
-          <br/>
- 
+    <div className="box">
+            <h1 className="title"> Projects </h1>
+            <section class = "desc">
+            Will update as I gain more experience and create new things. </section>
+    <div className = "wrapper">
+              <div className="card">
+          <h1>Smart Game AI</h1>
+          <p className = "proj-desc"> Java </p>
+          <a href="https://github.com/seyelence/cs310/tree/main/pa3" rel="noreferrer">
+          GitHub </a>
         </div>
+      
+        <div className="card">
+        <h1>Portfolio </h1>
+        <p className = "proj-desc"> ReactJS, HTML, CSS </p>
+        <a href="https://github.com/seyelence/cs310/tree/main/pa3" rel="noreferrer">
+        GitHub </a>
       </div>
+      </div>
+        </div>
       </section>
   );
 }
